0bfa9bea95b823da12d16481b86f936cbbb68c56
AleksTret <Aleks.Tret@gmail.com>
2020-10-03 19:19:28 +0300

    feat: add check()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports =    function check(bracketSequence, bracketsConfig){
+  if(!bracketSequence || !bracketsConfig || !Array.isArray(bracketsConfig))
+    return false;
+
+  let sequence = bracketSequence.split("");
+  if (sequence.length % 2) 
+    return false;
+
+  let bracketsSet = new Map(bracketsConfig);
+  let result = [];
+
+  sequence.forEach((item) => {
+    if(item === bracketsSet.get(result[result.length - 1])){
+      result.pop();
+    }
+    else {
+      result.push(item);
+    }
+  });
+  
+  return result.length === 0;
 }
